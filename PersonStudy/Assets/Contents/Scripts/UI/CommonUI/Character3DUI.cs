9d66d133105e4a5095af455fa951d3ad3eb5edbc
harrywkd5300 <harrywkd5300@gmail.com>
2022-12-11 15:03:27 +0900

    #권오현 - 3DUI 추가
    ~ 3DUI 추가( Render Tex 로 렌더링 )

---
+++ PersonStudy/Assets/Contents/Scripts/UI/CommonUI/Character3DUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character3DUI : UIBase
+{
+	[SerializeField] GameObject obj_3D = null;
+
+	public override void Init()
+	{
+	}
+	public override void Clear()
+	{
+	}
+	public override bool Shuts()
+	{ 
+		return Shuts();
+	}
+	public override void SetActive( bool value )
+	{
+		base.SetActive( value );
+	}
+}
